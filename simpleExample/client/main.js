b7dbfd08cba7527fb39c4e40652347cf82e370d6
mk_atix <malic@atixlabs.com>
2017-04-20 15:14:00 -0300

    Sample app.

---
+++ simpleExample/client/main.js
@@ -0,0 +1,47 @@
+import { Meteor } from 'meteor/meteor';
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+
+import './main.html';
+
+Template.hello.onCreated(function helloOnCreated() {
+});
+
+Template.hello.helpers({
+  lineUser() {
+    const user = Meteor.users.findOne({ _id: Meteor.userId() });
+
+    if (user) {
+      return user.services.line.id;
+    } else {
+      return '';
+    }
+  },
+});
+
+Template.hello.events({
+  'click .line-login'(event, instace) {
+    Meteor.loginWithLine(function (err, res) {
+      console.log('login callback', err, res);
+      if (err !== 'undefined') {
+        console.log('sucess ' + res, err);
+      } else {
+        console.log('login failed ' + err);
+      }
+    });
+  },
+  'click .message-button'(event, instance) {
+    event.preventDefault();
+    const lineId = $(".message-user").val();
+    const message = $(".message-text").val();
+    console.log('message', message, lineId);
+    Meteor.call('sendLineMessage', message, lineId, (err, res) => {
+      if (res) {
+        alert('Message send!');
+      }
+    });
+  },
+  'click .logout'(event, instance) {
+    Meteor.logout();
+  }
+});
